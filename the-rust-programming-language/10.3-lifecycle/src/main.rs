7af43219f116172ac88a358c7617008e25ce5739
Cphayim <i@cphayim.me>
2023-04-25 15:44:44 +0800

    feat(the-book/10.3): lifecycle

---
+++ the-rust-programming-language/10.3-lifecycle/src/main.rs
@@ -0,0 +1,32 @@
+use std::fmt::Display;
+
+fn longest<'ab>(x: &'ab str, y: &'ab str) -> &'ab str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn main() {
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+
+    let ann = "Hello".to_string();
+    longest_with_an_announcement(string1.as_str(), string2, &ann);
+}
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}

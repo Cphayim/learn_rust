9a272818be7409ce9c15f2ef6e0d614e01c039da
Cphayim <i@cphayim.me>
2023-05-08 14:24:53 +0800

    feat(the-book/16.1): thread

---
+++ the-rust-programming-language/16.1-thread/src/main.rs
@@ -0,0 +1,28 @@
+use std::{thread, time::Duration};
+
+fn main1() {
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    // handle.join() 会阻塞当前线程直到 handle 对应的线程执行完毕
+    handle.join().unwrap();
+}
+
+fn main() {
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || {
+        println!("Here is a vector: {:?}", v);
+    });
+
+    handle.join().unwrap();
+}
